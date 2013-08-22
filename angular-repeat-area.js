1d697e83e0dc03cb052b8d906beafe43a34c02a0
Marcus Dalgren <marcus.dalgren@gmail.com>
2013-08-22 23:35:22 +0200

    First commit

---
+++ angular-repeat-area.js
@@ -0,0 +1,18 @@
+﻿(function (window, angular, undefined) {
+	'use strict';
+	angular.module('ngRepeatArea', ['ng']).directive('ngRepeatArea', function () {
+		// return the directive link function. (compile function not needed)
+		return {
+			scope:    true,
+			restrict: 'EACM',
+			priority: 2000,
+			compile:  function (tElement, tAttrs, transclude) {
+				var children = tElement.children();
+				var itemsToRemove = children.length - 1;
+				for (var i = 0; i < itemsToRemove; i++) {
+					children.eq(i).remove();
+				}
+			}
+		}
+	});
+})(window, window.angular);
